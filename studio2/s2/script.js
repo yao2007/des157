36e44b596f5b0514efc809c8bcd6d3fbfef9aa8d
YueYao <yueyao@ucdavis.edu>
2018-02-06 14:25:17 -0800

    attemp to submit

---
+++ studio2/s2/script.js
@@ -0,0 +1,37 @@
+
+
+var results = document.getElementById("results");
+
+
+document.makeup.onsubmit = processForm;
+document.makeup.onreset = resetForm;
+
+function resetForm() {
+  myMsg.innerHTML = "";
+  results.className="hide";
+}
+// define process functionS
+function processForm() {
+    var food1 = document.makeup.food1.value;
+    var food2 = document.makeup.food2.value;
+    var food3 = document.makeup.food3.value;
+    var food4 = document.makeup.food4.value;
+    var food5 = document.makeup.food5.value;
+
+    var myMsg = document.getElementById("myMsg");
+
+    results.className="show";
+    
+    myMsg.innerHTML =
+    ["Hi guys! So today I woke up and I literally wanted to do my makeup using ONLY the food that I have in my "+kitchen+" – just to see if I could do it! Considering I only used "+food1+ ", "+food2+", "+food3+", "+food4+" and "+food5+", I think it turned out ok! If you guys want to create your own full makeup look using the food you have in your house, make sure to use the hashtag: #foodmakeupchallenge so I can see!!"];
+
+      
+      return false;
+  }
+
+  
+
+  
+
+
+
--- studio2/script.js
+++ studio2/script.js
@@ -1,53 +1,109 @@
-console.log("reading");
+// practice listening for events and then
+// calling (or invoking) anonymous and named functions
 
-// store form fields in variables
-var userName = document.f.userName.value;
-var userColor = document.f.userColor.value;
-var results = document.getElementById("results");
-var myMsg = document.getElementById("myMsg");
+// capture button into a variable
+var btn=document.getElementById('btn');
+var init=document.getElementById('init');
+var pen=document.getElementById('pen');
+var bottle=document.getElementById('bottle');
+var glass=document.getElementById('glass');
 
-// capture the submit event
-document.f.onsubmit = processForm;
+// mouseover listener that changes the
+// background color with a call to an anonymous function
+btn.addEventListener ('mouseover', function() {
+  btn.style.backgroundColor='orange';
+});
 
-// prompt1: capture the reset event and send it a callback function named resetForm
-document.f.onreset = resetForm;
+init.addEventListener ('mouseover', function() {
+  init.src='s2/FaceQ1517950674603.png';
+});
 
-function resetForm() {
-  myMsg.innerHTML = "";
-  results.className="hide";
+pen.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950759714.png';
+   pen.style.backgroundColor='indigo';
+});
+bottle.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950496503.png';
+   bottle.style.backgroundColor='indigo';
+});
+glass.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950469964.png';
+   glass.style.backgroundColor='indigo';
+});
+
+pen.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950759714.png';
+   pen.style.backgroundColor='indigo';
+});
+bottle.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950496503.png';
+   bottle.style.backgroundColor='indigo';
+});
+glass.addEventListener('dblclick',
+function(){
+   init.src='s2/FaceQ1517950469964.png';
+   glass.style.backgroundColor='indigo';
+});
+// mouseup mouseout that calls a named (custom)
+// function that changes the background color
+init.addEventListener('mouseout',mouseWentOut);
+
+
+
+// defined custom function
+function mouseWentOut(){
+   init.src='s2/FaceQ1517950309557.png';
 }
-// define process functionS
-function processForm() {
-  userName = document.f.userName.value;
-  userColor = document.f.userColor.value;
-  // easter egg
-  switch (userColor) {
-    case "blue":
-      myMsg.style.backgroundColor = "blue";
-      break;
-    case "orange":
-      myMsg.style.backgroundColor = "orange";
-      break;
-    default:
-      myMsg.style.backgroundColor = "#333";
-  }
 
-  myMsg.innerHTML =
-    "Hi, " +
-    userName +
-    "!<br>You love the color <em>" +
-    userColor +
-    "</em>!  Have a great day!";
-  
-  results.className="show";
-  //prevent page from reloading
-  return false;
+// note: practice typing the following
+// by looking at the sample code provided
+// but do not just copy and paste it...type it
+
+// prompt1: add a mousedown listener that calls an
+// anonymous function to change the
+// background color to green
+btn.addEventListener('mousedown',
+function(){
+   btn.style.backgroundColor='green';
+});
+// prompt2: add a mouseup listener that calls a
+// named function, "mouseUpChange" to change the
+// background color to blue
+btn.addEventListener('mouseup',mouseUpChange);
+
+// prompt3: define the named funcion "mouseUpChange"
+function mouseUpChange(){
+   btn.style.backgroundColor='blue';
 }
+// prompt4: add a dblclick listener that calls an
+// anonymous function to change the
+// background color to indigo
+btn.addEventListener('dblclick',
+function(){
+   btn.style.backgroundColor='indigo';
+});
+// challenge: have more fun: add innerHTML content, add other css c
 
-/* 
-prompt2: 
-define the resetForm function to:
-  1. set the myMsg.innerHTML to an empty string
-  2. set the class attribute for myMsg to "hide"
-*/
 
+   var cat = document.getElementById('cat');
+   var statusdiv = document.getElementById('statusdiv');
+   var counter = 0; //counts how many times the cat is touched
+
+//Prompt 1: add a listener that changes the status to "Ouch" within the touchstart event
+   cat.addEventListener('touchstart', function(e){
+      //e.preventDefault();
+  //    var touches = e.changedTouches;
+      statusdiv.innerHTML="Ouch";
+   }, false);
+
+//Prompt 2: add another listener that increment the counter and show in the status how many times the cat was touched (touchend)
+    cat.addEventListener("touchend", function(e){
+       //e.preventDefault();
+       counter++;
+       statusdiv.innerHTML=counter;
+    }, false);
