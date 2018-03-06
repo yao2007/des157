60ab23c7064fc4891b8995bed2f840dec692da07
YueYao <yueyao@ucdavis.edu>
2018-03-06 11:43:00 -0800

    bug

--- proto1/script.js
+++ proto1/script.js
@@ -11,3 +11,17 @@ function openTab1(evt, tab1Name) {
     document.getElementById(tab1Name).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function openTab2(evt, tab1Name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab1Name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
