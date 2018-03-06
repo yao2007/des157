33fd0eef327a158710144519990b4044bba78fd1
YueYao <yueyao@ucdavis.edu>
2018-03-06 11:14:21 -0800

    po

---
+++ proto1/script.js
@@ -0,0 +1,13 @@
+function openTab1(evt, tab1Name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab1Name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
