ddc9e475ccca613766f6d43a22787603781bbe67
YueYao <yueyao@ucdavis.edu>
2018-03-06 16:35:44 -0800

    merge proto1

--- proto1/script.js
+++ proto1/script.js
@@ -25,3 +25,25 @@ function openTab2(evt, tab1Name) {
     document.getElementById(tab1Name).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function redirect() {
+    var url = document.getElementById("ForLink").value;
+    window.open("url");
+  }
+
+function redirect2() {
+    var x = document.getElementById("ForText").value;
+    // document.getElementById("demo").innerHTML = x;
+    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+    myWindow.document.write("<p>"+ x + "</p>");
+    myWindow.document.write("<p style='width:100%;'>");
+    myWindow.document.write("<select id='cboprintSize' onchange='document.getElementById(\"divContainer\").style.width = document.getElementById(\"cboprintSize\").value + \"px\";'><option value=\"300\">A4</option><option value=\"500\">A5</option></select>");
+    myWindow.document.write("<div id='divContainer' style='width:500; height:700; background-color:white'>");
+    myWindow.document.write("<table width='100%'><tr><td>");
+    myWindow.document.write(document.getElementById('printForm').innerHTML);
+    myWindow.document.write("</td></tr><table>");
+    myWindow.document.write("</div>");
+    myWindow.document.write("</div>");
+    myWindow.document.close();
+    myWindow.focus();
+}
