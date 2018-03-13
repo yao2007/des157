df0d37b459ccc5beb568681d7182e951170de859
YueYao <yueyao@ucdavis.edu>
2018-03-13 13:08:02 -0700

    update text area

--- proto2/script.js
+++ proto2/script.js
@@ -47,3 +47,12 @@ function redirect2() {
     myWindow.document.close();
     myWindow.focus();
 }
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
