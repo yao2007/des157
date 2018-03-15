75bd90f2bdac06d07ea10a96e3a4fdf1dab68d8d
YueYao <yueyao@ucdavis.edu>
2018-03-15 16:55:16 -0700

    about update

--- proto2/script.js
+++ proto2/script.js
@@ -26,17 +26,34 @@ function openTab2(evt, tab1Name) {
     evt.currentTarget.className += " active";
 }
 
+// broke this string down so you don't have to scroll
+var newurl = "https://mercury.postlight.com/parser?" +
+             "url=http://www.businessinsider.com/" +
+             "joel-spolsky-stack-exchange-interview-2016-12";
+
+// set your api key
+var apiKey = "qkqEY2g2zCZci9HmVxQ5RnY5zUrYY0j2Kbwk342m";
+$.ajax({
+    url: newurl,
+    dataType: "json",
+    beforeSend: function(xhr){xhr.setRequestHeader('x-api-key', apiKey);},
+    success: function(data){
+        console.log(data.title);
+    }
+})
+
 function redirect() {
     //var url = document.getElementById("ForLink").value;
     //window.open("https://mercury.postlight.com/web-parser/");
-    setTimeout(() => window.open('https://mercury.postlight.com/web-parser/'), 30);
+    //console.log("Hello Print!");
+    setTimeout(() => window.open('newurl'), 30);
   }
 
 function redirect2() {
     var x = document.getElementById("ForText").value;
     //document.getElementById("demo").innerHTML = x; //result.html
     var myWindow = window.open("", "MsgWindow", "width=200,height=100");
-    myWindow.document.write("<p>"+ x + "</p>");
+    myWindow.document.write("<p>"+ x + console.log(data.title) + "</p>");
     myWindow.document.write("<p style='width:100%;'>");
     myWindow.document.write("<select id='cboprintSize' onchange='document.getElementById(\"divContainer\").style.width = document.getElementById(\"cboprintSize\").value + \"px\";'><option value=\"300\">A4</option><option value=\"500\">A5</option></select>");
     myWindow.document.write("<div id='divContainer' style='width:500; height:700; background-color:white'>");
