e7c40f90d610e73f077cd0655d7a10bbb7decb38
YueYao <yueyao@ucdavis.edu>
2018-03-21 13:39:54 -0700

    imp update

--- final/script.js
+++ final/script.js
@@ -1,3 +1,68 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight');
+			} else {
+		    label.removeClass('highlight');
+			}
+    } else if (e.type === 'focus') {
+
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight');
+			}
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});
+
+function init() {
+    // Retrieving the text input's value which was stored into a cookie
+    setTimeout(function(){ alert("Welcome to this online text font editor for dyslexia. No need to install fonts, just enter links or paragraphs, and you can instantly use the fonts that are easy for you to read anytime, anywhere. "); }, 1);
+    //document.write("<div>"+"<p style='font-family:OpenDyslexic3-Bold,sans-serif;'>"+ "passed value = "+mytext+"</p>"+"</div>");
+    //document.getElementById("mytext").style.fontFamily = "OpenDyslexic-Regular,sans-serif";
+}
+var bt = document.getElementById("buttonT");
+bt.addEventListener("mouseover", warntip);
+bt.addEventListener("mouseout", closewarntip);
+var tipw;
+function warntip(){
+  tipw=setTimeout(tipalert(), 1);
+}
+
+function closewarntip(){
+  clearTimeout(tipw);
+}
+function tipalert(){
+  alert("Since this is an asynchronous request and technical limitation, the link must be clicked twice");
+}
+
 function openTab1(evt, tab1Name) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -26,42 +91,74 @@ function openTab2(evt, tab1Name) {
     evt.currentTarget.className += " active";
 }
 
-// broke this string down so you don't have to scroll
-var newurl = "https://mercury.postlight.com/parser?" +
-             "url=http://www.businessinsider.com/" +
-             "joel-spolsky-stack-exchange-interview-2016-12";
 
-// set your api key
-var apiKey = "qkqEY2g2zCZci9HmVxQ5RnY5zUrYY0j2Kbwk342m";
-$.ajax({
-    url: newurl,
-    dataType: "json",
-    beforeSend: function(xhr){xhr.setRequestHeader('x-api-key', apiKey);},
-    success: function(data){
-        console.log(data.title);
-    }
-})
 
 function redirect() {
     //var url = document.getElementById("ForLink").value;
     //window.open("https://mercury.postlight.com/web-parser/");
     //console.log("Hello Print!");
-    setTimeout(() => window.open('newurl'), 30);
+    //setTimeout(() => window.open('newurl'), 30);
+    var link = document.getElementById("ForLink").value;
+    // broke this string down so you don't have to scroll
+    var newurl = "https://mercury.postlight.com/parser?" +
+                 "url=" + link ;
+                 //link="http://www.businessinsider.com/joel-spolsky-stack-exchange-interview-2016-12"
+    // set your api key
+    var apiKey = "qkqEY2g2zCZci9HmVxQ5RnY5zUrYY0j2Kbwk342m";
+    var output = "";
+    $.ajax({
+            url: newurl,
+            dataType: "json",
+
+            beforeSend: function(xhr){
+                console.log(apiKey);
+                xhr.setRequestHeader('x-api-key', apiKey);
+            },
+
+            success: function(data){
+                console.log("debugging")
+                console.log(data.title);
+                output=data.title;
+                datatitle(data.title);
+                databody(data.content);
+              //  $("h1").html(data.title);
+                return data;
+            },
+            error: function (error) {
+                console.log(error)
+                alert("An error occured" );
+            }
+        })
+
+    var myWindow= window.open("", "MsgWindow", "width=200,height=100");
+    //setTimeout(() => myWindow =window.open("", "MsgWindow", "width=200,height=100"), 0.01);
+    //myWindow.close();
+    opennewwindow(myWindow);
+
   }
+function opennewwindow(myWindow){
+  myWindow = window.open("", "MsgWindow", "width=200,height=100");
+  //myWindow.document.write("<p>"+ console.log(data.title) + "</p>");
+  myWindow.document.write('<html><head><title>Result</title><link rel="stylesheet" type="text/css" href="css/styles.css"></head><body>');
+  myWindow.document.write("<div><h1>"+datatitle+"</h1>"+"<p>"+ databody+"</p></div>");
+  myWindow.document.write('</body></html>');
+}
+var datatitle;
+function datatitle(title){
+  datatitle =title;
+}
+var databody;
+function databody(e){
+  databody=e;
+}
 
 function redirect2() {
     var x = document.getElementById("ForText").value;
     //document.getElementById("demo").innerHTML = x; //result.html
     var myWindow = window.open("", "MsgWindow", "width=200,height=100");
-    myWindow.document.write("<p>"+ x + console.log(data.title) + "</p>");
-    myWindow.document.write("<p style='width:100%;'>");
-    myWindow.document.write("<select id='cboprintSize' onchange='document.getElementById(\"divContainer\").style.width = document.getElementById(\"cboprintSize\").value + \"px\";'><option value=\"300\">A4</option><option value=\"500\">A5</option></select>");
-    myWindow.document.write("<div id='divContainer' style='width:500; height:700; background-color:white'>");
-    myWindow.document.write("<table width='100%'><tr><td>");
-    myWindow.document.write(document.getElementById('printForm').innerHTML);
-    myWindow.document.write("</td></tr><table>");
-    myWindow.document.write("</div>");
-    myWindow.document.write("</div>");
+    myWindow.document.write('<html><head><title>Result</title><link rel="stylesheet" type="text/css" href="css/styles.css"></head><body>');
+    myWindow.document.write("<div><p>"+ x +"</p></div>");
+    myWindow.document.write('</body></html>');
     myWindow.document.close();
     myWindow.focus();
 }
